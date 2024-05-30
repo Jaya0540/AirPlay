935f73dfa2906db92dee679e359cbc67ad6a8a0f
Jaya0540 <jayakrishna.cheemala@logituit.com>
2024-05-30 11:26:53 +0530

    Initial Commit

---
+++ AirPlay/AirPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirPlayApp.swift
+//  AirPlay
+//
+//  Created by Jai on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AirPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirPlay
+//
+//  Created by Jai on 30/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
